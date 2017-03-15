769d5d3afca721fc26b5e9e6c3aefd98b3bcdb5c
David <davidporter33@icloud.com>
2017-03-15 16:06:15 -0700

    completed the tasks required for lab1 for resubmit. will be getting my cert signed tomorrow

--- test/arithmetic.js
+++ test/arithmetic.js
@@ -4,7 +4,19 @@ const expect = require('chai').expect;
 const arithmetic = require('../lib/arithmetic.js');
 
 describe('testing module arithmetic.js', function() {
-  it('should return a number', function (){
-    expect(arithmetic).to.equal.number;
+  it('should return a number', function () {
+    expect(arithmetic.add(1,2)).to.equal(3);
+    expect(arithmetic.add(3,4)).to.equal(7);
+  });
+  it('should error', function() {
+    expect(arithmetic.add('', 4)).to.equal('You need to enter a number');
+  });
+  it('should return a number', function () {
+    expect(arithmetic.subtract(4,3)).to.equal(1);
+    expect(arithmetic.subtract(2,1)).to.equal(1);
+  });
+  it('should error', function() {
+    expect(arithmetic.subtract('', 4)).to.equal('You need to enter a number');
+    expect(arithmetic.subtract('', 2)).to.equal('You need to enter a number');
   });
 });
